69b183380de500d66957e19905e995a896a23b0e
MS <mswebmaster@users.noreply.github.com>
2021-05-18 11:21:49 +0200

    Update and rename password_paste.js to password_paste.user.js

--- password_paste.user.js
+++ password_paste.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name        Login password paste - paytweak.com
-// @namespace   Violentmonkey Scripts
 // @match       https://*paytweak.com/login
 // @grant       none
-// @version     1.0
-// @author      -
-// @description 08/04/2021 à 14:32:03
+// @version     1.1
+// @description 18/05/2021 à 11:21:00
 // ==/UserScript==
 const inputPassword = document.querySelector('form[name="login"] input[type="password"]');
 inputPassword.removeAttribute('onpaste');
