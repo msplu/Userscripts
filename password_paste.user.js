a60260a1f396ca77bbda5f17ed679752de45b5e6
MS <mswebmaster@users.noreply.github.com>
2021-05-18 11:26:34 +0200

    Update password_paste.user.js

--- password_paste.user.js
+++ password_paste.user.js
@@ -2,9 +2,10 @@
 // @name        Login password paste - paytweak.com
 // @match       https://*paytweak.com/login
 // @grant       none
-// @version     1.1
-// @description 18/05/2021 à 11:21:00
+// @version     1.2
+// @description 18/05/2021 à 11:26:00
 // ==/UserScript==
+
 const inputPassword = document.querySelector('form[name="login"] input[type="password"]');
 inputPassword.removeAttribute('onpaste');
 inputPassword.removeAttribute('oncopy');
