11e8e41fc4b361a9eca62a6952b2cac052e1a7af
MS <mswebmaster@users.noreply.github.com>
2021-05-18 11:31:19 +0200

    Update cookies_no_consent.user.js

--- cookies_no_consent.user.js
+++ cookies_no_consent.user.js
@@ -4,8 +4,6 @@
 // @grant       none
 // @version     1.2
 // @description 18/05/2021 à 11:24:00
-// @updateURL   https://raw.githubusercontent.com/mswebmaster/cookies-no-consent/main/cookies_no_consent.js
-// @downloadURL https://raw.githubusercontent.com/mswebmaster/cookies-no-consent/main/cookies_no_consent.js
 // ==/UserScript==
 
 function removeModal() {
