e13937db4ed371cc99ff31745e14b8a0dba2b0db
MS <mswebmaster@users.noreply.github.com>
2021-05-18 11:07:03 +0200

    Update cookies_no_consent.js

--- cookies_no_consent.js
+++ cookies_no_consent.js
@@ -2,6 +2,7 @@
 // @name        Cookies No Consent
 // @match       *://*/*
 // @grant       none
+// @version     1.0
 // @description 18/05/2021 à 11:04:00
 // @updateURL   https://raw.githubusercontent.com/mswebmaster/cookies-no-consent/main/cookies_no_consent.js
 // @downloadURL https://raw.githubusercontent.com/mswebmaster/cookies-no-consent/main/cookies_no_consent.js
